4a7bc1927162a9a25412ce3a98dbbab06ee62121
Aaron Brethorst <aaron@brethorsting.com>
2024-02-12 22:02:32 -0800

    WIP: Parameterize DB Credentials
    This is failing at the very end: for some subtle and undoubtedly simple reason, it's failing to properly invoke the `exec` line at the end of oba_entrypoint.sh, `exec "/usr/local/tomcat/bin/catalina.sh run"`
    `

--- oba_config/oba_entrypoint.sh
+++ oba_config/oba_entrypoint.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
+set -eux
 
 # Your custom commands here
 echo "ABXOXO Running custom startup commands..."
 
-# Now call the parent image's CMD
-exec catalina.sh run
+# onebusaway-transit-data-federation-webapp
+
+sed -i \
+    "s|#JDBC_DRIVER#|${JDBC_DRIVER}|g" \
+    /usr/local/tomcat/webapps/onebusaway-transit-data-federation-webapp/WEB-INF/classes/data-sources.xml
+
+sed -i \
+    "s|#JDBC_URL#|${JDBC_URL}|g" \
+    /usr/local/tomcat/webapps/onebusaway-transit-data-federation-webapp/WEB-INF/classes/data-sources.xml
+
+sed -i \
+    "s|#JDBC_USERNAME#|${JDBC_USERNAME}|g" \
+    /usr/local/tomcat/webapps/onebusaway-transit-data-federation-webapp/WEB-INF/classes/data-sources.xml
+
+sed -i \
+    "s|#JDBC_PASSWORD#|${JDBC_PASSWORD}|g" \
+    /usr/local/tomcat/webapps/onebusaway-transit-data-federation-webapp/WEB-INF/classes/data-sources.xml
+
+# onebusaway-api-webapp
+
+sed -i \
+    "s|#JDBC_DRIVER#|${JDBC_DRIVER}|g" \
+    /usr/local/tomcat/webapps/onebusaway-api-webapp/WEB-INF/classes/data-sources.xml
+
+sed -i \
+    "s|#JDBC_URL#|${JDBC_URL}|g" \
+    /usr/local/tomcat/webapps/onebusaway-api-webapp/WEB-INF/classes/data-sources.xml
+
+sed -i \
+    "s|#JDBC_USERNAME#|${JDBC_USERNAME}|g" \
+    /usr/local/tomcat/webapps/onebusaway-api-webapp/WEB-INF/classes/data-sources.xml
+
+sed -i \
+    "s|#JDBC_PASSWORD#|${JDBC_PASSWORD}|g" \
+    /usr/local/tomcat/webapps/onebusaway-api-webapp/WEB-INF/classes/data-sources.xml
+
+# y u no work?
+exec "/usr/local/tomcat/bin/catalina.sh run"
